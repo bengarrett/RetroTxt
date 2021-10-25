60d595431b81328f5730f8fd7d036e00696851e4
Ben Garrett <code.by.ben@gmail.com>
2021-10-25 15:51:20 +1100

    Removed redundant this.id = tab.id

--- ext/scripts/sw/action.js
+++ ext/scripts/sw/action.js
@@ -23,26 +23,26 @@ class Action {
    */
   constructor(tab = {}) {
     this.scheme = ``
-    this.id = tab.id
-    this.info = tab
+    this.tab = tab
     this.state = false
-    if (`url` in this.info)
-      this.scheme = `${this.info.url.split(`:`)[0].toLowerCase()}`
-    else this.info.url = `activeTab.Tab.url permission denied`
-    if (!(`title` in this.info))
-      this.info.title = `activeTab.Tab.title permission denied`
+    if (`url` in this.tab)
+      this.scheme = `${this.tab.url.split(`:`)[0].toLowerCase()}`
+    else this.tab.url = `activeTab.Tab.url permission denied`
+    if (!(`title` in this.tab))
+      this.tab.title = `activeTab.Tab.title permission denied`
   }
   /**
    * Browser tab selected and activated.
    */
   activated() {
-    if (this.id === ``) return console.warn(`action actived tab ID is empty.`)
-    const key = `${SessionKey}${this.id}`
+    if (this.tab.id === ``)
+      return console.warn(`action actived tab ID is empty.`)
+    const key = `${SessionKey}${this.tab.id}`
     chrome.storage.local.get(`${key}`, (store) => {
       if (Object.entries(store).length === 0)
-        return Console(`Ignore tab ID ${this.id}: ${this.info.title}`)
-      Console(`★ Activated tab ID ${this.id}: ${this.info.title}`)
-      const button = new ToolbarButton(this.id),
+        return Console(`Ignore tab ID ${this.tab.id}: ${this.tab.title}`)
+      Console(`★ Activated tab ID ${this.tab.id}: ${this.tab.title}`)
+      const button = new ToolbarButton(this.tab.id),
         textfile = store[key].textfile
       this.state = false
       switch (textfile) {
@@ -87,16 +87,16 @@ class Action {
       if (Developer in store) DeveloperMode = true
     })
 
-    if (!(`url` in this.info) || this.info.url.length === 0)
+    if (!(`url` in this.tab) || this.tab.url.length === 0)
       return console.warn(
         `RetroTxt could not determine the URL of the active tab #%s.`,
-        this.id
+        this.tab.id
       )
 
     if (DeveloperMode)
-      console.log(`↩ Toolbar button click registered for tab #%s.`, this.id)
+      console.log(`↩ Toolbar button click registered for tab #%s.`, this.tab.id)
 
-    chrome.tabs.sendMessage(this.id, { id: `invoked` }, () => {
+    chrome.tabs.sendMessage(this.tab.id, { id: `invoked` }, () => {
       if (CheckLastError(`action click invoked send message`)) return
     })
   }
