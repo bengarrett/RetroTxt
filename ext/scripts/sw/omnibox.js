903256622a85c23af0302621bcd3ad9af78d634d
Ben <code.by.ben@gmail.com>
2022-09-01 11:54:44 +1000

    omnibox autoruns with exact typed matches.

--- ext/scripts/sw/omnibox.js
+++ ext/scripts/sw/omnibox.js
@@ -1,6 +1,8 @@
 // File: scripts/sw/omnibox.js
 //
 // Browser address bar omnibox input to handle terminal like commands.
+// https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/omnibox
+// https://developer.chrome.com/docs/extensions/reference/omnibox/
 
 chrome.runtime.onInstalled.addListener(() => {
   ConsoleLoad(`omnibox.js`)
@@ -8,7 +10,7 @@ chrome.runtime.onInstalled.addListener(() => {
 
 /**
  * Handle browser address bar omnibox commands.
- * To trigger type `rt ` (rt space).
+ * To trigger type `txt ` (txt space).
  * @class Omnibox
  */
 class Omnibox {
@@ -16,12 +18,12 @@ class Omnibox {
     this.keys = []
     const suggestions = new Map()
       .set(`version`, `RetroTxt - version information`)
-      .set(`fonts`, `RetroTxt - font selections`)
-      .set(`display`, `RetroTxt - text, ansi and color display options`)
+      .set(`fonts`, `RetroTxt - font selection`)
+      .set(`display`, `RetroTxt - text, ANSI and color display options`)
       .set(`settings`, `RetroTxt - settings`)
-      .set(`documentation`, `RetroTxt - online documentation`)
+      .set(`documentation`, `RetroTxt - read the online documentation`)
       .set(`credits`, `RetroTxt - credits`)
-      .set(`samples`, `RetroTxt - sample ANSI and ASCII artwork links`)
+      .set(`samples`, `RetroTxt - sample ANSI and ASCII artworks`)
       .set(`useful`, `RetroTxt - useful and related links`)
     this.suggestions = suggestions
     this._keys()
@@ -31,25 +33,33 @@ class Omnibox {
    */
   async startup() {
     try {
+      // Defines the first suggestion that appears in the drop-down when the user enters the keyword for your extension, followed by a space.
       chrome.omnibox.setDefaultSuggestion({
         description: `RetroTxt commands: ${this.keys.join(`, `)}`,
       })
     } catch (e) {
       return CheckError(`RetroTxt failed to load the Omnibox.`, false)
     }
-    chrome.omnibox.onInputChanged.addListener((text, addSuggestions) => {
+    // Fired whenever the user's input changes, after they have focused the address bar and typed your extension's omnibox keyword, followed by a space.
+    chrome.omnibox.onInputChanged.addListener((text, suggest) => {
       Console(`⌨ Omnibox input: ${text}`)
-      addSuggestions(this._getMatchingProperties(text))
+      suggest(this._getMatchingProperties(text))
+      // the ominbox does not display anything with extact matches
+      // so instead this will auto-launch those matches
+      const match = this.suggestions.get(text)
+      if (typeof match === `undefined`) return
+      if (match.length > 0) return OpenOptions(text)
     })
+    // Fired when the user accepts one of your extension's suggestions.
     chrome.omnibox.onInputEntered.addListener((text, disposition) => {
       Console(`⌨ Entered in ${disposition}: ${text}`)
       if (disposition === `currentTab`) return OpenOptions(text)
     })
   }
-  _getMatchingProperties(input) {
+  _getMatchingProperties(text) {
     const result = []
     for (const prompt of this.keys) {
-      if (prompt.indexOf(input) === 0) {
+      if (prompt.startsWith(text)) {
         result.push({
           content: prompt,
           description: this.suggestions.get(prompt),
